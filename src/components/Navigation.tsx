3b18f3ab04472adb7f03a497475aeee3f1e428c3
Ivan Kostianovsky Vasconsellos <ivankostianovsky@gmail.com>
2025-12-17 20:20:13 -0300

    Remove mobile register button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Menu, X } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import ThemeToggle from "@/components/ThemeToggle";
 
 const Navigation = () => {
